be6b20732f7008bce6d48458f89950f13696202e
Jasmin Li <jl6ww@virginia.edu>
2019-06-03 09:30:36 -0400

    first commit

---
+++ src/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./App.css";
+import firebase from "./firebase.js"
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+  render() {
+    return;
+  }
+}
+
+export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXhrhvYUYRSkrKjnXvNH6rBq999TrYu5s",
+  authDomain: "contract-bidding.firebaseapp.com",
+  databaseURL: "https://contract-bidding.firebaseio.com",
+  projectId: "contract-bidding",
+  storageBucket: "contract-bidding.appspot.com",
+  messagingSenderId: "18871062059",
+  appId: "1:18871062059:web:65357280efb87cb6"
+};
