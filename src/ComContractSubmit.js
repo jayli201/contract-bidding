39605477c037e2e970e17a875c3bd6348bd86f05
AsmirS <asmir.h.shaikh@gmail.com>
2019-06-03 16:17:07 -0400

    finished styless contract submit

--- src/ComContractSubmit.js
+++ src/ComContractSubmit.js
@@ -1,35 +1,50 @@
 import React from 'react';
+import firebase from 'firebase';
+import firebaseConfig from './firebase'
 
 export default class ComContractSubmit extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            value : ''
+            codename : '',
+            codecompany : '',
+            codedetails : '',
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({
-            value : event.target.value 
+    handleChange() {
+        let nameinput = document.getElementById('name').value
+        let companyinput = document.getElementById('company').value
+        let detailsinput = document.getElementById('details').value
+        console.log(nameinput)
+        console.log(companyinput)
+        console.log(detailsinput)
+
+        var contractRef = firebase.database().ref('Contracts')
+        var userPosted  = contractRef.push({
+            name : nameinput,
+            company : companyinput,
+            details : detailsinput,
         })
-        console.log(this.state.value)
     }
+    
+    
     render() {
         return(
             <div>
 
         <form onSubmit={this.handleSubmit}>
-        <h2>Company Name</h2>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-        <h2>Contract Name</h2>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-        <h2>Company Details</h2>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-        <h2>Submit</h2>
-        <input type="submit" value="Submit" />
-      </form>
+                <h2>Enter Name: </h2>
+                <input type = 'text' id = 'name'/>
+                <h2>Enter Company Name: </h2>
+                <input type = 'text' id = 'company'/>
+                <h2>Enter Contract Details: </h2>
+                <input type = 'text' id = 'details'/>
+                <h2>Press Submit When Finished </h2>
+                <button type = 'button' onClick = {this.handleChange} >Submit</button>
+        </form>
 
 
 
