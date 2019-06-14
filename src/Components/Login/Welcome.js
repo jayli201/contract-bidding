886d54046d90f711f1e90fd6668ba905af3a4617
Jasmin Li <jl6ww@virginia.edu>
2019-06-14 11:57:27 -0400

    fixed about

--- src/Components/Login/Welcome.js
+++ src/Components/Login/Welcome.js
@@ -1,135 +1,3 @@
-<div className="welcome" style={{ background: "white" }}>
-  <Row className="welcome">
-    <Col span={3} />
-    <Col span={15}>
-      <PageHeader
-        className="welcome"
-        style={{
-          background: "white",
-          textAlign: "left"
-        }}
-      >
-        <NavLink to="/">
-          <img src="images/logo.png" width="175" height="50" />
-        </NavLink>
-      </PageHeader>
-    </Col>
-    <Col span={3}>
-      <PageHeader style={{ background: "white" }}>
-        <Menu style={{ borderBottom: "transparent" }}>
-          <Menu.Item>
-            <NavLink
-              to="/login"
-              style={{
-                color: "green",
-                fontWeight: "bold"
-              }}
-            >
-              LOGIN
-         </NavLink>
-          </Menu.Item>
-        </Menu>
-      </PageHeader>
-    </Col>
-    <Divider />
-  </Row>
-  {/* <Row>
-     <Col span={3} />
-     <Col span={18}>
-      <PageHeader style={{ background: "#389e0d" }} />
-     </Col>
-     <Col span={3} />
-    </Row> */}
-  <br />
-  <br />
-  <Row>
-    <div className="center">
-      <Content
-        style={{
-          borderStyle: "solid",
-          borderColor: "#237804",
-          fontSize: "25px"
-        }}
-      >
-        <br />
-        <h1
-          style={{
-            color: "#092b00"
-          }}
-        >
-          We
-       </h1>
-        <h1
-          style={{
-            color: "#092b00"
-          }}
-        >
-          Think
-        {/* <Icon type="bulb" theme="twoTone" twoToneColor="#fadb14" /> */}
-        </h1>
-        <h1
-          style={{
-            color: "#237804"
-          }}
-        >
-          Together
-       </h1>
-        <br />
-      </Content>
-    </div>
-    <br />
-    <br />
-    <h1
-      className="text"
-      style={{
-        color: "#092b00"
-      }}
-    >
-      RevTek makes communicating easier so you can start focusing on
-      projects sooner.
-     </h1>
-    <br />
-    <Button
-      type="primary"
-      icon="double-right"
-      onClick={() => {
-        this.props.history.push("/login");
-      }}
-    />
-  </Row>
-  <br />
-  <br />
-</div>
-Collapse
-
-  .parent {
-  display: flex;
-}
-
-.center {
-  display: flex;
-  /* flex-direction: column; */
-  /* align-items: center; */
-  align - items: center;
-  justify - content: center;
-  margin - left: 580px;
-  margin - right: 580px;
-  font - family: "Courier Bold", Rockwell, Courier, Georgia, Times,
-    "Times New Roman", serif;
-  font - weight: bold;
-}
-
-.text {
-  font - family: "Courier Bold", Rockwell, Courier, Georgia, Times,
-    "Times New Roman", serif;
-  font - weight: bold;
-}
-
-Jasmin Li[11: 27 AM]
-#237804
-
-Jasmin Li[11: 40 AM]
-Untitled
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { withRouter } from "react-router-dom";
@@ -203,6 +71,9 @@ class Welcome extends Component {
             <PageHeader style={{ background: "white" }}>
               <Menu mode="horizontal" style={{ borderBottom: "transparent" }}>
                 <Menu.Item>
+                  <NavLink to="/about">ABOUT</NavLink>
+                </Menu.Item>
+                <Menu.Item>
                   <NavLink to="/login">LOGIN</NavLink>
                 </Menu.Item>
                 <Menu.Item>
@@ -228,15 +99,16 @@ class Welcome extends Component {
             }}
           >
             <br />
-            <label className="title">
+            <label className="text">
               <h1 style={{ fontWeight: "bold" }}>We&ensp;think&ensp; </h1>
               <h1 style={{ color: "#237804", fontWeight: "bold" }}>
                 together.
-       </h1>
+              </h1>
             </label>
             {/* <Icon type="bulb" theme="twoTone" twoToneColor="#52c41a" /> */}
             <br />
           </div>
+          <br />
           <h1
             className="text"
             style={{
@@ -245,7 +117,7 @@ class Welcome extends Component {
           >
             RevTek makes communicating easier so you can start focusing on
             projects sooner.
-     </h1>
+          </h1>
           <br />
           <Divider />
           {/* <PageHeader style={{ background: "#237804" }} /> */}
@@ -294,6 +166,7 @@ class Welcome extends Component {
             <Col span={4} />
           </Content>
         </Row>
+        <br />
         <br />
         <br />
       </div>
